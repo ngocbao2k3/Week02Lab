5bdbda2a76d8f39673540a22af4485b7c3c552f0
BaoNguyen <bao.ng.nguyen76@gmail.com>
2017-09-20 13:50:11 -0600

    New Project Creation Created a new Java Web Application called AgeNextBirthday.
    Create Servlet
    Create a Servlet called AgeCalculatorServlet

--- src/java/sait/cprg352/AgeCalculatorServlet.java
+++ src/java/sait/cprg352/AgeCalculatorServlet.java
@@ -17,72 +17,51 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author 595770
  */
-@WebServlet(name = "AgeCalculatorServlet", urlPatterns = {"/AgeCalculatorServlet"})
+@WebServlet(name = "AgeCalculatorServlet", urlPatterns = {"/ageCal"})
 public class AgeCalculatorServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AgeCalculatorServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AgeCalculatorServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String currentAge = request.getParameter("age");
+        try
+        {
+        if(currentAge == null || currentAge.isEmpty())
+        {
+            request.setAttribute("errorMessage","Please enter your age");
+            request.setAttribute("currentAge",currentAge);
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+            
+        }
+        else if(Integer.parseInt(currentAge) > 0)
+        {
+            request.setAttribute("result", "Your age next birthday will be " + (Integer.parseInt(currentAge)+1));
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+            
+        }
+        else
+        {
+            request.setAttribute("errorMessage","Please enter only number");
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+            
+        }
+        }
+        catch (NumberFormatException e)
+        {
+            request.setAttribute("errorMessage","Please enter only number");
+             getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+        }
+        
+        
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
+   
+        
 }
